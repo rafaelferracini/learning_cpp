1f14b36289606557913d020a6f30236bd11fb729
rafaelferracini <121617955+rafaelferracini@users.noreply.github.com>
2026-07-24 00:36:23 -0300

    Compiling on linux
    Learning how to set neovim and compile c++ on linux

--- 1. introduction/main.cpp
+++ 1. introduction/main.cpp
@@ -1,70 +1,73 @@
 #include <iostream> // import input and output library
-#include <string> // import string data type
 #include <limits>
+#include <string> // import string data type
 
-void clear_buffer(){
-    /* 
-        cin.ignore(count, delim): count é o numero maximo de caracters descartados e delim é o ultimo caracter a ser descartado, quando o comando chegar nele, para.
-            - ignore ate count caracters ou até encontrar delim
-        std::numeric_limits<int>::max(): fornece informação sobre determinado tipo numerico (no caso int), no caso o valor maximo do int
-        std::numeric_limits<std::streamsize>::max(): fornece o valor maximo de stremsize (9223372036854775807), praticamente infinito. Garante que vai apagar o buffer inteiro
-    */
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+void clear_buffer() {
+  /*
+      cin.ignore(count, delim): count é o numero maximo de caracters descartados
+     e delim é o ultimo caracter a ser descartado, quando o comando chegar nele,
+     para.
+          - ignore ate count caracters ou até encontrar delim
+      std::numeric_limits<int>::max(): fornece informação sobre determinado tipo
+     numerico (no caso int), no caso o valor maximo do int
+      std::numeric_limits<std::streamsize>::max(): fornece o valor maximo de
+     stremsize (9223372036854775807), praticamente infinito. Garante que vai
+     apagar o buffer inteiro
+  */
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
-int main(int argc, char const *argv[])
-{
-    /*
-        std::cout : print data to the console
-        std::cin : read data from  the terminal
-        std::cerr : print erros to console
-        std::clog : print log massage to console
-    */
-    
-    
-    // Printing data
-    std::cout << "\n--------------------------------" << std::endl;
-    std::cout << "Hello World, C++20" << std::endl;
-    std::cout << "--------------------------------" << std::endl;
-    /*
-        Declaring and initiliazing variables
-            - int age=21;
-            - int age; followed by age = 21;
-    */
-    int age {21};
-    std::cout << "\n--------------------------------" << std::endl;
-    std::cout << "Age : " << age << std::endl;
-    std::cout << "--------------------------------" << std::endl;
+int main(int argc, char const *argv[]) {
+  /*
+      std::cout : print data to the console
+      std::cin : read data from  the terminal
+      std::cerr : print erros to console
+      std::clog : print log massage to console
+  */
 
+  // Printing data
+  std::cout << "\n--------------------------------" << std::endl;
+  std::cout << "Hello World, C++20" << std::endl;
+  std::cout << "--------------------------------" << std::endl;
+  /*
+      Declaring and initiliazing variables
+          - int age=21;
+          - int age; followed by age = 21;
+  */
+  int age{21};
+  std::cout << "\n--------------------------------" << std::endl;
+  std::cout << "Age : " << age << std::endl;
+  std::cout << "--------------------------------" << std::endl;
 
-    // Input values to user to variables
-    int age1;
-    std::string name;
- 
-    std::cout << "\n--------------------------------" << std::endl;
-    std::cout << "Type your name and age separated by a space or enter: ";
-        //std::cin >> name;
-        //std::cin >> age1;
-    std::cin >> name >> age1;
-    std::cout << "Welcome " << name << " you're " << age1 << " years" << std::endl;
-    std::cout << "--------------------------------" << std::endl;
+  // Input values to user to variables
+  int age1;
+  std::string name;
 
-    // Theres a \n on buffer due to last cin. Clear buffer until the \n character
-    clear_buffer();
-    
-    // Input data with spaces
-    int age2;
-    std::string full_name;
-    std::cout << "\n--------------------------------" << std::endl;
-    std::cout << "Please enter your full name: ";
-    std::getline(std::cin, full_name);
-    std::cin >> age2;
-    std::cout << "Welcome " << full_name << " you're " << age2 << " years" << std::endl;
-    std::cout << "--------------------------------" << std::endl;
-    /* Error and log message
-        std::cerr << "This is an error message" << std::endl;
-        std::clog << "This is a log message" << std::endl;
-    */
-    return 0;
+  std::cout << "\n--------------------------------" << std::endl;
+  std::cout << "Type your name and age separated by a space or enter: ";
+  // std::cin >> name;
+  // std::cin >> age1;
+  std::cin >> name >> age1;
+  std::cout << "Welcome " << name << " you're " << age1 << " years"
+            << std::endl;
+  std::cout << "--------------------------------" << std::endl;
+
+  // Theres a \n on buffer due to last cin. Clear buffer until the \n character
+  clear_buffer();
+
+  // Input data with spaces
+  int age2;
+  std::string full_name;
+  std::cout << "\n--------------------------------" << std::endl;
+  std::cout << "Please enter your full name: ";
+  std::getline(std::cin, full_name);
+  std::cin >> age2;
+  std::cout << "Welcome " << full_name << " you're " << age2 << " years"
+            << std::endl;
+  std::cout << "--------------------------------" << std::endl;
+  /* Error and log message
+      std::cerr << "This is an error message" << std::endl;
+      std::clog << "This is a log message" << std::endl;
+  */
+  return 0;
 }
-
--- 2. variables_and_datatypes/main.cpp
+++ 2. variables_and_datatypes/main.cpp
@@ -12,23 +12,20 @@ using namespace std;
     auto: later
 
 */
-int main(int argc, char const *argv[])
-{
-    // Bases numéricas
-     int number1 = 15; // Decimal
-     int number2 = 017; // Octal
-     int number3 = 0x0f; // Hexadecimal
-     int number4 = 0b00001111; // Binary 
+int main(int argc, char const *argv[]) {
+  // Bases numéricas
+  int number1 = 15;         // Decimal
+  int number2 = 017;        // Octal
+  int number3 = 0x0f;       // Hexadecimal
+  int number4 = 0b00001111; // Binary
 
-     cout << number1 << endl;
-     cout << number2 << endl;
-     cout << number3 << endl;
-     cout << number4 << endl;
+  cout << number1 << endl;
+  cout << number2 << endl;
+  cout << number3 << endl;
+  cout << number4 << endl;
 
-     /* Integers
-        - Occupy 4 bytes
-     */
-     int number {13};
-
+  /* Integers
+     - Occupy 4 bytes
+  */
+  int number{13};
 }
-
