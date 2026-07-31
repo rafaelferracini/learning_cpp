73e0c5b410deccca76a46f75d6862c7fb960b81e
rafaelferracini <121617955+rafaelferracini@users.noreply.github.com>
2026-07-31 11:59:55 -0300

    aula01

---
+++ aula01/src/segundo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int cenouras;
+  cenouras = 25;
+
+  cout << "Eu tenho " << cenouras << " cenouras. " << endl;
+
+  cenouras = cenouras - 1;
+  cout << "Agora eu tenho " << cenouras << " cenouras." << endl;
+
+  return 0;
+}
