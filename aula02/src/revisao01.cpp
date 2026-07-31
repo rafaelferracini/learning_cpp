c462c8cd78fb9f7ce9526ad535138d15e19f631d
rafaelferracini <121617955+rafaelferracini@users.noreply.github.com>
2026-07-31 14:44:40 -0300

    exercicios_de_revisao

---
+++ aula02/src/revisao01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  cout << "Bem-vindo\n" << "ao mundo\n" << "do C++" << endl;
+
+  cout << "Bem-vindo" << endl << "ao mundo" << endl << "do C++";
+  return 0;
+}
